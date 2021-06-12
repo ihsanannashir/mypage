3599da14fcfca7a8cc367c19faac22f54f882f56
greatspy <ihsanannashir@gmail.com>
2021-06-12 23:44:00 +0700

    added interaction for landing

--- src/component/landing.jsx
+++ src/component/landing.jsx
@@ -9,9 +9,9 @@ class Landing extends React.Component {
                 <span id="judul">Hi, i'm Ihsan!</span><br/>
                 <span id="desc">a frontend development enthusiast.</span>
                 <div className="hub">
-                    <FontAwesomeIcon className='lkn' icon={faLinkedin} size="3x"/>
-                    <FontAwesomeIcon className='gh' icon={faGithub} size="3x" />
-                    <FontAwesomeIcon className='ig' icon={faInstagram} size="3x"/>
+                    <a href="http://linkedin.com/in/ihsanannashir"><FontAwesomeIcon className='lkn' icon={faLinkedin} size="3x"/></a>
+                    <a href="http://github.com/ihsanannashir"><FontAwesomeIcon className='gh' icon={faGithub} size="3x" /></a>
+                    <a href="http://instagram.com/ihsanannashir"><FontAwesomeIcon className='ig' icon={faInstagram} size="3x"/></a>
                     <div className="cv">CV</div>
                 </div>
             </div>
