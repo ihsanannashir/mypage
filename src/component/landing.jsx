9c7d28e05f51d22b1697e32c1902cfeb72bf99a8
greatspy <ihsanannashir@gmail.com>
2021-06-12 23:23:29 +0700

    landing page mockup finished, need interaction

--- src/component/landing.jsx
+++ src/component/landing.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../css/bio.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-//ini class component
 class Landing extends React.Component {
     render() {
-        return <div><h1>ini Landing</h1></div>
+        return <div className="bio">
+                <span id="judul">Hi, i'm Ihsan!</span><br/>
+                <span id="desc">a frontend development enthusiast.</span>
+                <div className="hub">
+                    <FontAwesomeIcon className='lkn' icon={faLinkedin} size="3x"/>
+                    <FontAwesomeIcon className='gh' icon={faGithub} size="3x" />
+                    <FontAwesomeIcon className='ig' icon={faInstagram} size="3x"/>
+                    <div className="cv">CV</div>
+                </div>
+            </div>
     }
 }
 
